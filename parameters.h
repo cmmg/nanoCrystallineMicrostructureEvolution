982766f026f2cf184c4d66ece7b563e64b49a3b0
pandeyy <ppandey7@wisc.edu>
2019-07-23 16:15:31 -0500

    omly parameters change

--- parameters.h
+++ parameters.h
@@ -2,7 +2,7 @@
 #define DIMS 3
 #define problemWidth 10
 #define problemHeight 100
-#define NumMeshPoints 4
+#define NumMeshPoints 3
 #define refinementFactor 1
 
 //mechanics properties
@@ -13,7 +13,7 @@
 #define TotalTime 1000*TimeStep
 
 //solver controls
-#define isDirectSolver true
+#define isDirectSolver false
 
 //output controls
 #define outputFileName "solution"
@@ -24,8 +24,8 @@
 #define PI 3.14159265
 #define self_hardening  0.0180
 #define Ss 0.0148
-#define n_seed_points 70
-#define n_diff_grains 5
+#define n_seed_points 100
+#define n_diff_grains 10
 #define totalDOF n_diff_grains+DIMS
 #define InterfaceEnergyParameter {1.0e-3, 1.0e-3, 1.0e-3} //{Kx, Ky, Kz}
 //#define InterfaceEnergyParameter {2, 2, 2} //{Kx, Ky, Kz}
