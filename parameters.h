75c39d9de6660c73916991a986de6323dfd2bf87
pandeyy <ppandey7@wisc.edu>
2019-09-03 15:05:52 -0500

    Gronaghen model of solute drag added

--- parameters.h
+++ parameters.h
@@ -1,36 +1,29 @@
 //problem geometry, mesh control
-#define DIMS 3
-#define problemWidth 10
-#define problemHeight 100
-#define NumMeshPoints 3
-#define refinementFactor 1
+#define DIMS 2
+#define problemWidth 1.0
+#define refinementFactor 7
 
-//mechanics properties
-#define elasticModulus 4//2.0e11
-#define PoissonsRatio 0.3
-#define TimeStep 0.1
+//phase field properties
+#define InterfaceEnergyParameter 5.0e-3//2*8.0e-1// {1.0e-2, 1.0e-2, 1.0e-2} //{Kx, Ky, Kz}
+//#define dFdC  400*c[q]*(c[q]-1.0)*(c[q]-0.5) //derivative of the free energy
+#define Mobility 0.1//3.72e-4//0.5//*3.0e-12
+#define M_gb     1e-4//2.64e-11//0.04
+#define M_alpha  1.0e-4 //0.001
+
 //time step controls
+#define TimeStep 5.0e-4
 #define TotalTime 1000*TimeStep
 
-//solver controls
-#define isDirectSolver false
-
+#define n_phase 1
+#define n_solute 1
+#define TotalDOF n_phase+n_solute
+#define Vm 1.0//7.0e-6 //0.001
+#define WA 10//1088//1000
+#define WB -2300.7615//-1000//900
+#define gas_constant 8.314
+#define temperature 900
+#define GA 0
+#define GB 0
+//Vm is molar volume
 //output controls
 #define outputFileName "solution"
-
-
-//#define Yield_stress 0.1
-#define n_slip_systems 1
-#define PI 3.14159265
-#define self_hardening  0.0180
-#define Ss 0.0148
-#define n_seed_points 40
-#define n_diff_grains 4
-#define totalDOF n_diff_grains+DIMS
-#define InterfaceEnergyParameter {1.0e-3, 1.0e-3, 1.0e-3} //{Kx, Ky, Kz}
-//#define InterfaceEnergyParameter {2, 2, 2} //{Kx, Ky, Kz}
-#define L1 0.2
-#define alpha1 1
-#define beta1 1
-#define gamma1 1
-
